a8e91503fe02a1313d6f1bfb7babb105b04a7a0a
lstalero21 <ls.talero21@uniandes.edu.co>
2016-09-25 20:37:52 -0500

    Iteracion4, Ciclo1 - Test retorno numero elementos String vacio (RED)

--- src/test_statistics.py
+++ src/test_statistics.py
@@ -5,6 +5,7 @@ __author__ = 'Luis_Sebastian_Talero'
 
 
 class TestStatistics(TestCase):
+
     # Iteración 1
 
     #   def test_gen_statistics_1_0(self):
@@ -41,21 +42,27 @@ class TestStatistics(TestCase):
     #        array = [5, 2]
     #        self.assertEqual(Statistics.gen_statistics("2,3,4,5,6"), array, "String +2")
 
-    # Iteración 2
+    # Iteración 3
 
-    def test_gen_statistics_3_0(self):
-        array = [0, 0, 0]
+    #    def test_gen_statistics_3_0(self):
+    #        array = [0, 0, 0]
+    #        self.assertEqual(Statistics.gen_statistics(""), array, "Empty String")
+
+    #    def test_gen_statistics_3_1(self):
+    #        array = [1, 1, 1]
+    #        self.assertEqual(Statistics.gen_statistics("1"), array, "String 1")
+
+    #    def test_gen_statistics_3_3(self):
+    #        array = [2, 1, 2]
+    #        self.assertEqual(Statistics.gen_statistics("1,2"), array, "String 2")
+
+    #    def test_gen_statistics_3_3(self):
+    #        array = [5, 2, 6]
+    #        self.assertEqual(Statistics.gen_statistics("2,3,4,5,6"), array, "String +2")
+
+    # Iteración 4
+
+    def test_gen_statistics_4_0(self):
+        array = [0, 0, 0, 0]
         self.assertEqual(Statistics.gen_statistics(""), array, "Empty String")
-
-    def test_gen_statistics_3_1(self):
-        array = [1, 1, 1]
-        self.assertEqual(Statistics.gen_statistics("1"), array, "String 1")
-
-    def test_gen_statistics_3_3(self):
-        array = [2, 1, 2]
-        self.assertEqual(Statistics.gen_statistics("1,2"), array, "String 2")
-
-    def test_gen_statistics_3_3(self):
-        array = [5, 2, 6]
-        self.assertEqual(Statistics.gen_statistics("2,3,4,5,6"), array, "String +2")
 
