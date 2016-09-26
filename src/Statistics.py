83e1b5738ba7ca672f40e9fb2a0684eb3fc156f9
lstalero21 <ls.talero21@uniandes.edu.co>
2016-09-25 20:47:02 -0500

    Iteracion4, Ciclo2 - Test retorno numero elementos String un numero (GREEN)

--- src/Statistics.py
+++ src/Statistics.py
@@ -13,6 +13,16 @@ class Statistics:
             result_array.append(0)
             result_array.append(0)
             return result_array
-
+        else:
+            nums = sequence.split(",")
+            nums_int = list(map(int, nums))
+            min_number = min(i for i in nums_int if i >= 0)
+            max_number = max(i for i in nums_int if i >= 0)
+            if nums.__len__() == 1:
+                result_array.append(1)
+                result_array.append(min_number)
+                result_array.append(max_number)
+                result_array.append(mean(nums_int))
+                return result_array
 
 
