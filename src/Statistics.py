48da8ba40b68dc0e500366ea613fc26abd811610
lstalero21 <ls.talero21@uniandes.edu.co>
2016-09-25 19:33:54 -0500

    Iteracion2, Ciclo2 - Se corrige metodo para que Test retorno numero elementos String vacio funcione (GREEN)

--- src/Statistics.py
+++ src/Statistics.py
@@ -4,9 +4,10 @@ __author__ = 'Luis_Sebastian_Talero'
 class Statistics:
     @staticmethod
     def gen_statistics(sequence):
-        result_array = [0]
+        result_array = [1]
         if not sequence:
             result_array[0] = 0
+            result_array[1] = 0
             return result_array
         else:
             nums = sequence.split(",")
