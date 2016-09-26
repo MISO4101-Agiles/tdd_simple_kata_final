729831b17c66858031676c37ad254db3dec440dd
lstalero21 <ls.talero21@uniandes.edu.co>
2016-09-25 19:07:59 -0500

    Iteracion1, Ciclo2 - Test retorno numero elementos String un numero (RED)

--- src/test_statistics.py
+++ src/test_statistics.py
@@ -8,3 +8,5 @@ class TestStatistics(TestCase):
     def test_gen_statistics(self):
         self.assertEqual(Statistics.gen_statistics(""), 0, "Empty String")
 
+    def test_gen_statistics_1_1(self):
+        self.assertEqual(Statistics.gen_statistics("1"), 1, "Empty String")
